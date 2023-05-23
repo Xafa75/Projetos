d41ae079721f0ba0bef6d3305c71683b767da28e
Anthony Carvalho <110208691+Xafa75@users.noreply.github.com>
2023-05-23 19:35:04 -0300

    primeiro js
    uhuuu

---
+++ Projects/README.md
@@ -0,0 +1,2 @@
+# Projetos
+Olá, meu nome é Anthony, e esse é meu primeiro repositório no GitHub, sinta-se livre para explorar meus projetos!
---
+++ Projects/notas.js
@@ -0,0 +1,6 @@
+var nota1 = document.querySelector("#nota1").value;
+var nota2 = document.querySelector("#nota2").value;
+if (nota1 > 10) {
+    document.write("nota mt alta")
+}
+
