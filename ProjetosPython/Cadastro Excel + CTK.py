9098424eae9907b12676f43fb7145933793c91dc
Anthony Carvalho <anthonyflu20003@gmail.com>
2024-06-24 17:11:13 -0300

    campo de pesquisa

--- ProjetosPython/Cadastro Excel + CTK.py
+++ ProjetosPython/Cadastro Excel + CTK.py
@@ -7,12 +7,87 @@ from tkinter import ttk
 import openpyxl.workbook
 import openpyxl
 from openpyxl import Workbook,load_workbook
+import re
 
 #Adicionar:
+
 #Versionamento
+
 #Pagina de pesquisa
+
 #armazenar na pasta documentos
+
 #fazer backup
+global janelaPesquisa
+
+def pesquisa():
+    janelaMenu.withdraw()
+    JanelaPesquisa = s.CTkToplevel(janelaMenu)
+    JanelaPesquisa.geometry("800x400") #Definindo o tamanho inicial da janela
+    JanelaPesquisa.title("Clientes Cadastrados") #Definindo nome da janela
+    JanelaPesquisa.resizable(False,False)
+    s.set_appearance_mode("Dark")  
+    s.set_default_color_theme("dark-blue")
+    LabelPesquisa = s.CTkLabel(JanelaPesquisa,text="Pesquisa:")
+    LabelPesquisa.grid(row=1,column=1, pady=10, padx=14)
+
+    def buscar(nomePesquisado):
+        try:
+            PesquisaNome.set("")
+        except:
+            df = pd.read_excel('data.xlsx',sheet_name = 'Sheet') #guarda o arquivo excel em um dataframe
+            df2 = df[df['nome'].str.contains(nomePesquisado)]
+            print(df2)
+            linhaInicio = 2
+            for i in range(len(df2)):
+                print(i)
+                linhaDF = df2.iloc[i]
+
+                pesquisaDatavenda = s.CTkLabel(JanelaPesquisa, text=linhaDF['primeiroCadastro'])
+                pesquisaDatavenda.grid(column=7, row=linhaInicio)
+
+                pesquisaTelefone = s.CTkLabel(JanelaPesquisa, text=linhaDF['telefone'])
+                pesquisaTelefone.grid(column=6, row=linhaInicio, padx=10)
+
+                pesquisaFormaPG = s.CTkLabel(JanelaPesquisa, text=linhaDF['formaPagamento'])
+                pesquisaFormaPG.grid(column=5, row=linhaInicio, padx=5)
+
+                pesquisaValor = s.CTkLabel(JanelaPesquisa, text=linhaDF['pago'])
+                pesquisaValor.grid(column=4, row=linhaInicio, padx=10)
+
+                pesquisaDatavenda = s.CTkLabel(JanelaPesquisa, text=linhaDF['dataVenda'])
+                pesquisaDatavenda.grid(column=3, row=linhaInicio)
+
+                pesquisaServicos = s.CTkLabel(JanelaPesquisa, text=linhaDF['servicos'])
+                pesquisaServicos.grid(column=2, row=linhaInicio)
+
+                PesquisaNome = s.CTkLabel(JanelaPesquisa, text=linhaDF['nome'])
+                PesquisaNome.grid(column=1, row=linhaInicio)
+
+
+                linhaInicio += 1
+
+                print(linhaDF['nome'])
+
+
+    EntryPesquisa = s.CTkEntry(JanelaPesquisa, placeholder_text="Pesquisar...", border_color="Pink",width=300)
+    EntryPesquisa.grid(row=1,column=2, padx=10)
+
+    BTNBusca = s.CTkButton(JanelaPesquisa,text="Buscar",text_color="Black", fg_color="Pink",command=lambda:buscar(EntryPesquisa.get()))
+    BTNBusca.grid(row=1,column=3,padx=10)
+
+    #BTNVoltar = s.CTkButton(JanelaPesquisa,text="Voltar",command=lambda:Voltar(JanelaPesquisa))
+    #BTNVoltar.grid(row=2,column=1)
+
+    
+
+
+
+def Voltar(j):
+    janelaMenu.deiconify()
+    j.withdraw()
+
+
 
 
 def EnvioExcel(nome,servicos,dataVenda,pago,formaPagamento,telefone,primeiroCadastro): #Definindo a função
@@ -48,6 +123,7 @@ def EnvioExcel(nome,servicos,dataVenda,pago,formaPagamento,telefone,primeiroCada
             statusvar.set("Cadastrado com sucesso!")
         except:
             statusvar.set("Feche o Excel!")
+            
     EntryNome.delete(0,"end")
     EntryValor.delete(0,"end")
     EntryNumero.delete(0,"end")
@@ -103,5 +179,10 @@ ChkVisita.grid(row=2, column=4)
     
 BTNSalvar = s.CTkButton(janelaMenu,text="Salvar", text_color="Black", fg_color="Pink",width=100,command=lambda:EnvioExcel(EntryNome.get(),EntryOBS.get("1.0", "end-1c"),data,EntryValor.get(),OPTPagamento.get(),EntryNumero.get(), ChkVisita.get())) #Botão salvar
 BTNSalvar.grid(row=6,column=5)
+
+
+BTNVer = s.CTkButton(janelaMenu,text="Pesquisar",text_color="Black", fg_color="Pink",command=pesquisa,width=100) #Botao "Ver cadastros"
+BTNVer.grid(row=6,column=4,padx=5)
+
 
 janelaMenu.mainloop()
