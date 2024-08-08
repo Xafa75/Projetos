b9622c65ef7e073825e9551dac03233ca01a276d
Anthony Carvalho <anthonyflu20003@gmail.com>
2024-08-08 17:16:04 -0300

    dataframe

--- ProjetosPython/datajud.py
+++ ProjetosPython/datajud.py
@@ -1,45 +1,42 @@
 import requests
 import json
 import re
-import pprint
+import time
+import pandas as pd
 
 api_key = "ApiKey cDZHYzlZa0JadVREZDJCendQbXY6SkJlTzNjLV9TRENyQk1RdnFKZGRQdw=="
 url = "https://api-publica.datajud.cnj.jus.br/api_publica_tjrj/_search"
 headers = {'Authorization': api_key,
            'Content-Type': 'application/json'}
-
+listaProcessos = []
 def envio(sort):
     while True:
-        payload = json.dumps({
-        "size":5000,
-        "query":{"match" : {"assuntos.codigo": 3431}},
-
-        "sort":[{"dataAjuizamento":{ "order" : "asc" }}],
-        "search_after": [ sort ]
-        })
-
-        response = requests.request("POST",url,headers=headers, data=payload)
+        try:
+            payload = json.dumps({
+            "size":5000,
+            "query":{"match" : {"assuntos.codigo": 3431}},
+            "sort":[{"dataAjuizamento":{ "order" : "asc" }}],
+            "search_after": [ sort ]
+            })
+            response = requests.request("POST",url,headers=headers, data=payload,timeout=10)
+        except requests.exceptions.ReadTimeout:
+            df = pd.DataFrame(listaProcessos, columns=('numeroProcesso','classe','orgao','strData', 'ultimaAtualizacao','formato','assuntos','sort'))
+            print(df)
+            break
         dadosBrutos = response.json()   
         dados_dict = dadosBrutos['hits']['hits']
         for i in dados_dict:
-            sort = i['sort']
-            orgao = i['_source']['orgaoJulgador']
+            orgao = i['_source']['orgaoJulgador']['nome']
             strData = i['_source']['dataAjuizamento']
             numeroProcesso = i['_source']['numeroProcesso']
-            if orgao['codigo'] == 8046:
-                vara = 'NOVA FRIBURGO 2 VARA CRIMINAL'
-            elif orgao['codigo'] == 8047:
-                vara = 'NOVA FRIBURGO 1 VARA CRIMINAL'
             assuntos = i['_source']['assuntos']
             formato = i['_source']['formato']['nome']
             ultimaAtualizacao = i['_source']['dataHoraUltimaAtualizacao']
             classe = i["_source"]['classe']['nome']
             padrao = re.compile(r'20[1-2]\d......')
             sort = i['sort'][0]
-            print(strData,classe, numeroProcesso, formato, ultimaAtualizacao,sort, assuntos)           
-        try:
-            envio(sort)
-        except:
-            break
+            print(numeroProcesso,classe,orgao,strData, ultimaAtualizacao,formato,assuntos,sort)
+            listaProcessos.append([numeroProcesso,classe,orgao,strData, ultimaAtualizacao,formato,assuntos,sort])
+
 envio(sort = 0)
 
